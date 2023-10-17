89570a1db407bf3fa20ab9cd9031c6d8ab332682
Eugene <eugene.gila@gmail.com>
2023-10-17 13:24:32 +0300

    create

---
+++ config/params.php
@@ -0,0 +1,18 @@
+<?php
+
+return [
+    'admin_email' => 'info@site.ru',
+    'site_name' => 'Name',
+    'pagination' => 2,
+
+    'smtp_host' => 'app.debugmail.io',
+    'smtp_auth' => true,
+    'smtp_port' => 25,
+    'smtp_username' => '',
+    'smtp_password' => '',
+    'smtp_secure' => 'tls', //ssl
+    'smtp_from_email' => 'info@site.ru',
+
+    ];
+
+
